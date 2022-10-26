18ea51b3c2d951128d24710789cf01dcfc6df608
tnolanmartin <47613726+tnolanmartin@users.noreply.github.com>
2022-10-26 05:03:09 -0400

    Add files via upload

---
+++ Youtube_downloader.py
@@ -0,0 +1,15 @@
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+from pytube import YouTube
+from sys import argv
+
+link = argv[1]
+yt = YouTube(link)
+
+print("Title: ", yt.title)
+
+print("Views: ", yt.views)
+
+yd = yt.streams.get_highest_resolution()
+
+yd.download('/Users/travismartin/Desktop')
